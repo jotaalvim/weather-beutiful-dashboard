fc565da9be8e697b381418002eae6f6b884cd2a6
jotaalvim <joaoafonsoalvim@gmail.com>
2025-07-30 23:35:29 +0100

    more graphs

--- fetchImages.py
+++ fetchImages.py
@@ -1,36 +1,49 @@
 import requests
 import re
 import os
-import time
 
-
+#os.system('rm images/continente_ilhas/*')
+#os.system('rm images/peninsula_iberica/*')
 
 ipma = "https://www.ipma.pt/"
 
-url = "https://www.ipma.pt/pt/maritima/sst/index.jsp?area=cont-e-ilhas"
-
-response = requests.get(url)
-
-images = re.findall(r'resources.www/transf/mar/mercator/sst-cont-e-ilhas-[^"]*', response.text )
-
-#os.system('rm images/*')
-#os.system('output.gif')
-
-counter = 0
-for i in images:
-    #time.sleep(0.3)
-    os.system("curl " + ipma + i + f" --output images/img-{str(counter).zfill(3)}.png")
-    counter += 1
 
 
-os.system('ffmpeg -framerate 15 -i img-%03d.png -c:v libx264 -pix_fmt yuv420p tempertura_mar.mp4')
-#os.system('ffmpeg -framerate 15 -pattern_type glob -i "images/*.png" -vf "palettegen" palette.png')
-#os.system('ffmpeg -framerate 15 -pattern_type glob -i "images/*.png" -i palette.png -lavfi "paletteuse" output.gif')
+# Temperatura da agua do mar
+
+continente_ilhas  = "https://www.ipma.pt/pt/maritima/sst/index.jsp?area=cont-e-ilhas"
+peninsula_iberica = "https://www.ipma.pt/pt/maritima/sst/index.jsp?area=iberia"
+
+response_continente_ilhas  = requests.get(continente_ilhas)
+response_peninsula_iberica = requests.get(peninsula_iberica)
+
+images_continente_ilhas_temperatura = re.findall(r'resources.www/transf/mar/mercator/sst-cont-e-ilhas-[^"]*', response_continente_ilhas.text )
+images_peninsula_iberica_tempertura = re.findall(r'resources.www/transf/mar/nemo/sst-iberia-[^"]*', response_peninsula_iberica.text )
+
+#counter = 0
+#for i in images_continente_ilhas_temperatura:
+#    #time.sleep(0.3)
+#    os.system("curl " + ipma + i + f" --output images/continente_ilhas/img-{str(counter).zfill(3)}.png")
+#    counter += 1
+#
+#counter = 0
+#for i in images_peninsula_iberica_tempertura:
+#    #time.sleep(0.3)
+#    os.system("curl " + ipma + i + f" --output images/peninsula_iberica/img-{str(counter).zfill(3)}.png")
+#    counter += 1
+#
+
+os.system('ffmpeg -framerate 15 -i images/continente_ilhas/img-%03d.png  -c:v libx264 -pix_fmt yuv420p temperatura_mar_continente_ilhas.mp4')
+os.system('ffmpeg -framerate 15 -i images/peninsula_iberica/img-%03d.png -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -pix_fmt yuv420p temperatura_mar_peninsula_iberica.mp4')
 
 
-cartasMetereologicas = "https://www.ipma.pt/pt/otempo/prev.numerica/index.jsp"
+# Precipitacão 
 
+precipitacao_continente_ilhas = "https://www.ipma.pt/pt/otempo/prev.numerica/index.jsp?selModelo=5&selParam=23&selNivel=-1&selArea=2"
 
+response_precipitacao = requests.post(precipitacao_continente_ilhas)
 
+images_continete_ilhas_precipitacao = re.findall(r'resources.www/data/previsao/numerica/cartas/20250730/[^/]*/[^\.]+.gif ',response_precipitacao.text)
 
+print(len(images_peninsula_iberica_tempertura), len(images_continente_ilhas_temperatura), len(images_continete_ilhas_precipitacao))
 #print(images, len(images))
